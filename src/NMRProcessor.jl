4c34500c47d3a3a8f8b235dc0cc5093bed60d6da
Marcel Utz <marcel.utz@gmx.net>
2026-08-25 18:23:04 +0200

    sign error in entropy

--- src/NMRProcessor.jl
+++ src/NMRProcessor.jl
@@ -266,7 +266,7 @@ phase correction for automatic (unsupervised) phase correction.
 function entropy(s::SpectData{T,1}) where {T<:Number}
     h= s.dat .|> real  .|> abs
     h/=sum(h)
-    return -sum(ent.(h))/length(h)
+    return sum(ent.(h))/length(h)
 end
 
 
